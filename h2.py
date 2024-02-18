f37e5e9d4c616172a820b6f61abbe8e1d93d3b32
MaybellineM <mmmedra4@cougarnet.uh.edu>
2024-02-17 19:59:49 -0600

    get and post method finished

--- h2.py
+++ h2.py
@@ -4,6 +4,9 @@ from mysql.connector import Error
 from sql import Create_connection
 from sql import execute_query
 from sql import execute_read_query
+import flask
+from flask import jsonify #function that turns python object to jyson 
+from flask import request
 
 
 #Create a connection to mysql database
@@ -15,7 +18,7 @@ conn = Create_connection(myCreds.conString,myCreds.userName,myCreds.password,myC
 tire_brand = 'Corsa'
 tire_model = 'Highway Terrain Plus'
 tire_loadrating = 115
-tire_speedrating = 'up to 112'
+tire_speedrating = 'up to 112 mph'
 tire_type = 'all-season tire'
 tire_stock = 8
 
@@ -24,4 +27,84 @@ query = "INSERT INTO Inventory(brand, model, loadrating, speedrating, tiretype, 
 values = (tire_brand, tire_model, tire_loadrating, tire_speedrating, tire_type, tire_stock)
 
 # Execute the query
-#execute_query(conn, query, values)Commented out because I already ran the query
+execute_query(conn, query, values)#Commented out because I already ran the query
+
+
+
+
+#API Side of the assignment
+app = flask.Flask(__name__) #Sets up the application , name of the backend server
+
+#GET method to return  all tires from inventory
+@app.route('/api/alltires', methods = ["GET"])
+def api_all_tires():
+    myCreds = creds.Creds()
+    conn = Create_connection (myCreds.conString, myCreds.userName, myCreds.password, myCreds.dbname)
+
+    sql = "SELECT * FROM Inventory"
+
+    tires = execute_read_query(conn,sql)
+
+    conn.close()
+    return jsonify(tires)
+
+
+#POST: This API should allow to add a new tire to inventory
+
+@app.route('/api/addtire', methods = ["POST"])
+
+def addnew_tire():
+    #Establish Connection
+    myCreds = creds.Creds()
+    conn = Create_connection (myCreds.conString, myCreds.userName, myCreds.password, myCreds.dbname)
+    
+    #Extract values
+    new_tire = request.get_json()  
+    brand = new_tire['brand']
+    model = new_tire['model']
+    loadrating = new_tire['loadrating']
+    speedrating = new_tire['speedrating']
+    tiretype = new_tire['tiretype']
+    stock = new_tire['stock']
+
+    new_query = "INSERT INTO Inventory(brand, model, loadrating, speedrating, tiretype, stock) VALUES (%s, %s, %s, %s, %s, %s)"
+    values = (brand,model,loadrating,speedrating,tiretype,stock)
+
+
+    new_tire = execute_query(conn, new_query, values)
+
+    conn.close()
+    return jsonify(new_tire)
+
+
+
+
+
+
+
+#PUT: This API should allow to update the stock column of a tire, provided a given id
+
+
+
+
+
+
+
+
+#DELETE: This API should allow to delete a tire, provided a given id
+
+
+
+
+
+
+
+
+
+app.run()
+'''
+
+ C I T A T I O N S 
+ https://www.discounttire.com
+
+'''
